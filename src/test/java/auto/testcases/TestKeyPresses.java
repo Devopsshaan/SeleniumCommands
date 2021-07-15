09c7a8ae365967a860b0a7f553549f6a6ed490fd
Devopsshaan <moh.shanz@gmail.com>
2021-07-15 17:22:41 +0530

    First Commit

---
+++ src/test/java/auto/testcases/TestKeyPresses.java
@@ -0,0 +1,23 @@
+package auto.testcases;
+
+import auto.pages.KeyPressesPage;
+import auto.pages.WelcomePage;
+import auto.utility.Init;
+import org.testng.annotations.Test;
+
+
+
+public class TestKeyPresses extends Init {
+
+    @Test
+    public void testKeyPresses() {
+        WelcomePage welcomePage = new WelcomePage(driver);
+        welcomePage.verifyWelcomePageTitle();
+        welcomePage.verifyWelcomePageHeader();
+        welcomePage.clickOnLink("Key Presses");
+
+        KeyPressesPage keyPressesPage = new KeyPressesPage(driver);
+        keyPressesPage.verifyKeyPressesPageHeader();
+        keyPressesPage.pressAnyKey();
+    }
+}
